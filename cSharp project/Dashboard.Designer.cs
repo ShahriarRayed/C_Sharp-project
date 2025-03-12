4fed9bff5785f266cd7660f3e9a44e5d80e56bc0
ShahriarRayed <127871325+ShahriarRayed@users.noreply.github.com>
2025-03-12 23:13:36 +0600

    Uploaded

---
+++ cSharp project/Dashboard.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace cSharp_project
+{
+    partial class Dashboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Dashboard));
+            pictureBox3 = new PictureBox();
+            dataGridView1 = new DataGridView();
+            lblDateTime = new Label();
+            timer1 = new System.Windows.Forms.Timer(components);
+            label1 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
+            pictureBox3.Location = new Point(1, -1);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(798, 587);
+            pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox3.TabIndex = 10;
+            pictureBox3.TabStop = false;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AllowUserToDeleteRows = false;
+            dataGridView1.AllowUserToResizeColumns = false;
+            dataGridView1.AllowUserToResizeRows = false;
+            dataGridView1.BackgroundColor = Color.MistyRose;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(547, 93);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.ReadOnly = true;
+            dataGridView1.Size = new Size(241, 419);
+            dataGridView1.TabIndex = 14;
+            dataGridView1.CellContentClick += dataGridView1_CellContentClick;
+            // 
+            // lblDateTime
+            // 
+            lblDateTime.AutoSize = true;
+            lblDateTime.Font = new Font("Segoe UI", 20.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblDateTime.Image = (Image)resources.GetObject("lblDateTime.Image");
+            lblDateTime.Location = new Point(1, 93);
+            lblDateTime.Name = "lblDateTime";
+            lblDateTime.Size = new Size(158, 37);
+            lblDateTime.TabIndex = 15;
+            lblDateTime.Text = "LIVE TIME :";
+            lblDateTime.Click += lblDateTime_Click;
+            // 
+            // timer1
+            // 
+            timer1.Interval = 1000;
+            timer1.Tick += timer1_Tick;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 21.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Image = (Image)resources.GetObject("label1.Image");
+            label1.Location = new Point(279, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(197, 40);
+            label1.TabIndex = 16;
+            label1.Text = "DASHBOARD";
+            // 
+            // Dashboard
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 585);
+            Controls.Add(label1);
+            Controls.Add(lblDateTime);
+            Controls.Add(dataGridView1);
+            Controls.Add(pictureBox3);
+            Name = "Dashboard";
+            Text = "Dashboard";
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private PictureBox pictureBox3;
+        private DataGridView dataGridView1;
+        private Label lblDateTime;
+        private System.Windows.Forms.Timer timer1;
+        private Label label1;
+    }
+}
